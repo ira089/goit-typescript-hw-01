896f8051b07ae888a7d4ce6d1ecb798e4b118fa6
ira089 <gvir2015@gmail.com>
2024-02-23 22:21:35 +0200

    TS-hw-01_start2

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 if (button && input) {
     button.addEventListener("click", () => {
-        concatenation(input.value, "hqwe!");
+        concatenation(input.value, "hello!");
     });
 }
 //# sourceMappingURL=index.js.map
